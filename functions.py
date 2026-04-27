b16e8bb94430e10d0ee334a52eba9c658f1300a4
pramod <peruripramoddevops@gmail.com>
2026-04-27 23:01:17 +0530

    Python

--- functions.py
+++ functions.py
@@ -8,16 +8,31 @@
 # res = mult(2, 3)
 # print(res)
 
-def mult(a, b, *args, **kwargs):
-    # *args -> pass any no of position arguments
-    # **kwargs -> pass any no of keyword arguments
+# def mult(a, b, *args, **kwargs):
+#     # *args -> pass any no of position arguments
+#     # **kwargs -> pass any no of keyword arguments
     
 
-    # print(a, b, args, kwargs)
-    print(f"a:{a}, b:{b}, args: {args}, kwargs: {kwargs}") #formal string
-    return a * b # default is None
+#     # print(a, b, args, kwargs)
+#     print(f"a:{a}, b:{b}, args: {args}, kwargs: {kwargs}") #formal string
+#     return a * b # default is None
 
-# a=4, b=5 are keyword arguments, as they contain keys and values
-# '3.0', 3 are called as postional arguments
-res = mult('3.0', 3, 4, 5)
-print(res)
+# # a=4, b=5 are keyword arguments, as they contain keys and values
+# # '3.0', 3 are called as postional arguments
+# res = mult('3.0', 3, 4, 5)
+# print(res)
+
+
+def calc(a, b, operation):
+    if operation == "add":
+        return a + b
+    if operation == "sub":
+        return a - b
+    
+values = input("Enter 2 numbers: ")
+operation = input("Enter operation to perform (add, sub): ")
+print(values)
+print(map(int, values))
+# res = calc(a, b, operation)
+
+
